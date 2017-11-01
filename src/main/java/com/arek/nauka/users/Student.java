4fee4f8467f1f89a51160faa658fa98dcd380f39
ablech <arek_b1993@poczta.fm>
2017-11-01 20:01:57 +0100

    created Student class

---
+++ src/main/java/com/arek/nauka/users/Student.java
@@ -0,0 +1,32 @@
+package com.arek.nauka.users;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Arek on 01.11.2017.
+ */
+@Component
+public class Student {
+    private int age;
+    private String name;
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        System.out.println("Age: " + age);
+        return age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        System.out.println("Name: " +name);
+        return name;
+
+    }
+
+}
