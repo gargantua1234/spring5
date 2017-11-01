453820e32324d5fbef9e0a9831550b2a9f082a64
ablech <arek_b1993@poczta.fm>
2017-11-01 19:52:58 +0100

    spring project structure created

---
+++ src/main/java/com/arek/nauka/Application.java
@@ -0,0 +1,17 @@
+package com.arek.nauka;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Arek on 01.11.2017.
+ */
+@Configuration
+@ComponentScan("com.arek.nauka")
+public class Application {
+
+    public static void main(String[] args) {
+
+
+    }
+}
